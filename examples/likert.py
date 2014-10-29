05bbfc1cf4d9e4a959e47f224ae0045d579d2c90
Corinne Horn <cehorn@stanford.edu>
2014-10-28 18:03:06 -0700

    an inventory of examples

--- examples/likert.py
+++ examples/likert.py
@@ -31,11 +31,3 @@ X, Y = glrm_ord.factors()
 A_hat = glrm_ord.predict() # glrm_pca.predict(X, Y) works too; returns decode(XY)
 ch = glrm_ord.convergence() # convergence history
 pplot([A, A_hat, A-A_hat], ["original", "glrm", "error"])
-
-# Now with missing data
-missing = list(product(range(int(0.25*m), int(0.75*m)), range(int(0.25*n), int(0.75*n))))
-
-glrm_ord_missing = GLRM(A, loss, regX, regY, k, missing)
-glrm_ord_missing.fit()
-A_hat = glrm_ord_missing.predict()
-pplot([A, missing, A_hat, A-A_hat], ["original", "missing", "glrm", "error"])
